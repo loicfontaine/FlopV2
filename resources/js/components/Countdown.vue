7a90c611f2f89c24cc015fc71e69269652da8b29
Nisa Azizi <nisa.azizi@heig-vd.ch>
2023-06-14 19:10:53 +0200

    essai image et video

--- resources/js/components/Countdown.vue
+++ resources/js/components/Countdown.vue
@@ -60,12 +60,12 @@ export default{
       selectedImage: null,
       audioUrl: null,
       message: '',
-      /* form: {
+      form: {
         video: null,
         image: null,
         message: '',
         audioBlob: null,
-      }, */
+      },
       data: [],
     };
   },
@@ -152,14 +152,17 @@ afficherChampsAudio(item) {
     uploadFiles() {
         const formData = new FormData();
         /* formData.append('audio', this.$refs.audio.files[0]); */
-        if (this.$refs.image && this.$refs.image.files && this.$refs.image.files.length > 0) {
+        /* if (this.$refs.image && this.$refs.image.files && this.$refs.image.files.length > 0) {
   formData.append('image', this.$refs.image.files[0]);
-}
-        formData.append('message', this.message);
+
         if (this.$refs.video && this.$refs.video.files && this.$refs.video.files.length > 0) {
   formData.append('video', this.$refs.video.files[0]);
-}
+} */
+        formData.append('message', this.message);
         formData.append('audioBlob', this.audioBlob);
+        const files = event.target.files;
+        formData.append('image', this.image);
+        formData.append('video', this.video)
        /*  console.log(formData.get('audio')); */
         console.log(formData.get('image'));
         console.log(formData.get('message'));
