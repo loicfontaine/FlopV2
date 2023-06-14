8d0cd20754bbfa66cf2c59e881386dfee9783359
Nisa Azizi <nisa.azizi@heig-vd.ch>
2023-06-14 23:51:50 +0200

    réactif

--- resources/js/components/Countdown.vue
+++ resources/js/components/Countdown.vue
@@ -91,10 +91,30 @@ methods: {
           countdown: null,
         }));
         this.updateCountdowns();
+        setInterval(this.updateCountdown, 1000);
       } catch (error) {
         console.error(error);
       }
     },
+    updateCountdown() {
+  const now = new Date();
+  this.data.forEach(item => {
+    const endDate = new Date(item.end_time);
+    const timeDiff = endDate - now;
+
+    if (timeDiff <= 0) {
+      item.countdown = 'Le délai est écoulé';
+    } else {
+      const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+
+      item.countdown = `${days}j ${hours}h ${minutes}m ${seconds}s`;
+    }
+  });
+},
+
     updateCountdowns() {
       setInterval(() => {
         const now = new Date();
