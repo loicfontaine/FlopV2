dbd3c6fadb952e2382650492d457a032c71cfb23
Loïc Fontaine <loic.fontaine@heig-vd.ch>
2023-06-14 15:58:48 +0200

    miss }

--- resources/js/components/Countdown.vue
+++ resources/js/components/Countdown.vue
@@ -43,7 +43,7 @@
 <script>
 import json from './../../../public/data/challenges.json';
 import axios from 'axios';
-export default {
+export default{
   data() {
     return {
       countdown: 0,
@@ -267,7 +267,9 @@ afficherChampsAudio(item) {
     },
     
   },
-};
+}
+
+}
 </script>
 
 
