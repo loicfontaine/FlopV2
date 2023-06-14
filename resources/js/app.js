5b455a3798fbf9f8d82eaef02936ab407c2db85a
Nisa Azizi <nisa.azizi@heig-vd.ch>
2023-06-15 00:43:40 +0200

    mount survey

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,10 @@ import App from "./components/Countdown.vue";
 
 import TestForm from "./components/TestForm.vue";
 
+import Survey from "./components/Survey.vue";
+
 const app = createApp(App);
+const survey = createApp(Survey);
 
 app.mount("#app");
+survey.mount("#survey");
