a473942a402d358ffa0d318223e5424c602521f3
Loïc Fontaine <loic.fontaine@heig-vd.ch>
2023-06-14 15:29:27 +0200

    remove all chat vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,20 +1,9 @@
 import { createApp } from "vue";
 
 import App from "./components/Countdown.vue";
-import Chat from "./components/Chat.vue";
+
 import TestForm from "./components/TestForm.vue";
 
 const app = createApp(App);
-const chatApp = createApp(Chat);
-const testFormApp = createApp(TestForm);
 
 app.mount("#app");
-// Enregistrer les composants
-/* app.component(
-    "chat-messages",
-    require("./components/ChatMessages.vue").default
-);
-app.component("chat-form", require("./components/ChatForm.vue").default); */
-
-chatApp.mount("#chat");
-testFormApp.mount("#test-form");
