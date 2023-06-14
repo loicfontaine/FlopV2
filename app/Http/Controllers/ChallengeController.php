b431f0c1055a0ef9a5bb5d1f2308d3c6fe703073
Loïc Fontaine <loic.fontaine@heig-vd.ch>
2023-06-14 14:24:29 +0200

    dd id winner

--- app/Http/Controllers/ChallengeController.php
+++ app/Http/Controllers/ChallengeController.php
@@ -137,6 +137,7 @@ class ChallengeController extends Controller
     // fonction qui permet de terminer un contest, la vue nous envoie l'id du gagnant, l'id de la participation et l'id du reward
     public function endContest(int $participation_id)
     {
+        dd($participation_id);
         $participation = Participation::findOrFail($participation_id);
         $challenge = Challenge::findOrFail($participation->challenge_id);
         // get la reward en fonction du challenge_id qui correspond à la participation
