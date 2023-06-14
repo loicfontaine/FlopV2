f949729dd4e20c2866b8b3c08306fa2231024a38
quinteirosm <quinteiros.m@gmail.com>
2023-06-14 21:01:52 +0200

    try fix route contest v6

--- app/Http/Controllers/ChallengeController.php
+++ app/Http/Controllers/ChallengeController.php
@@ -137,7 +137,7 @@ class ChallengeController extends Controller
     // fonction qui permet de terminer un contest, la vue nous envoie l'id du gagnant, l'id de la participation et l'id du reward
     public function endContest(Request $request)
     {
-        dd($request->input);
+        dd($request->input('participationGagnante'));
         $participation = Participation::findOrFail($participation_id);
         $challenge = Challenge::findOrFail($participation->challenge_id);
         // get la reward en fonction du challenge_id qui correspond à la participation
