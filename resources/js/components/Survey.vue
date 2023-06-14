70b43e903896da85a4651dc1cca35c3b4f446e0d
Nisa Azizi <nisa.azizi@heig-vd.ch>
2023-06-15 01:04:10 +0200

    ajt

--- resources/js/components/Survey.vue
+++ resources/js/components/Survey.vue
@@ -1,28 +1,11 @@
-<!-- <template>
-    <form method="post" action="{{ route('answer.store') }}" accept-charset="UTF-8">
-        @csrf
-        <div class="sondage">
-            <p id="duree">Il vous reste {{ $timeLeft }} minutes pour participer au sondage</p>
-
-            <h1>{{ $dernierSondagetitle }}</h1>
-            <p>{{ $dernierSondage.description }}</p>
-            <p>Choix:</p>
-
-            <div v-for="(reponse, index) in reponses" :key="index" id="conteneur-options">
-                <label>{{ reponse.title }}</label>
-                <input type="radio" :name="'options[]'" :value="reponse.id" required>
-            </div>
-            <br>
-
-            <input type="submit" value="Valider">
-        </div>
-    </form>
+<template>
+    <div>
+        <h1>Survey</h1>
+    </div>
 </template>
 
 <script>
-export default {
-    props: ['timeLeft', 'dernierSondage', 'reponses'],
-    // ...
-}
+    export default {
+        name: "Survey"
+    }
 </script>
- -->
