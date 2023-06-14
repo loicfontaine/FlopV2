534897b439f08118a735b008e69941596498321f
LeaTalon <79629352+kassandrawoods@users.noreply.github.com>
2023-06-14 09:35:58 +0200

    changements

--- resources/views/emission.blade.php
+++ resources/views/emission.blade.php
@@ -30,6 +30,7 @@ COMEDY CLUB | Couleur 3 Interact
     <button id='accesChat'>
         Accéder au chat
     </button>
+    <div id="chat"></div>
     <div class='chatTest hidden'>
         <div class='container-chat'>
             <div class='container-message'>
