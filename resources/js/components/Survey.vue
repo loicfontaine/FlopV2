44d7ada5b9322fc191509b689242a2eff5333b67
Nisa Azizi <nisa.azizi@heig-vd.ch>
2023-06-15 01:03:18 +0200

    rm

--- resources/js/components/Survey.vue
+++ resources/js/components/Survey.vue
@@ -1,40 +1,28 @@
-<template>
-    <div>
-      <p>Il vous reste {{ timeLeft }} minutes pour participer au sondage</p>
-      <h1>{{ dernierSondage.title }}</h1>
-      <p>{{ dernierSondage.description }}</p>
-      <p>Choix:</p>
-      <!-- Boucle sur les réponses -->
-      <div v-for="reponse in reponses" :key="reponse.id" id="conteneur-options">
-        <label>{{ reponse.title }}</label>
-        <input type="radio" name="options[]" :value="reponse.id" required>
-      </div>
-      <br>
-      <button @click="submitForm">Valider</button>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      timeLeft: {
-        type: Number,
-        required: true
-      },
-      dernierSondage: {
-        type: Object,
-        required: true
-      },
-      reponses: {
-        type: Array,
-        required: true
-      }
-    },
-    methods: {
-      submitForm() {
-        // Code pour soumettre le formulaire
-      }
-    }
-  }
-  </script>
-  
+<!-- <template>
+    <form method="post" action="{{ route('answer.store') }}" accept-charset="UTF-8">
+        @csrf
+        <div class="sondage">
+            <p id="duree">Il vous reste {{ $timeLeft }} minutes pour participer au sondage</p>
+
+            <h1>{{ $dernierSondagetitle }}</h1>
+            <p>{{ $dernierSondage.description }}</p>
+            <p>Choix:</p>
+
+            <div v-for="(reponse, index) in reponses" :key="index" id="conteneur-options">
+                <label>{{ reponse.title }}</label>
+                <input type="radio" :name="'options[]'" :value="reponse.id" required>
+            </div>
+            <br>
+
+            <input type="submit" value="Valider">
+        </div>
+    </form>
+</template>
+
+<script>
+export default {
+    props: ['timeLeft', 'dernierSondage', 'reponses'],
+    // ...
+}
+</script>
+ -->
