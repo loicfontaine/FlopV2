78a9bce5a658aed64b6f620a4a1669234dc410bf
Loïc Fontaine <loic.fontaine@heig-vd.ch>
2023-06-15 10:07:28 +0200

    dd to check where it goes

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -60,6 +60,7 @@ class FileController extends Controller
 
         if (Participation::where("user_id", "=", $userId)->where("challenge_id", "=", $request->input("challenge_id"))->first()) {
             session()->flash('error', 'Tu as déjà participé à ce défi !');
+            dd("nope");
             return redirect()->route("home");
         } else {
 
@@ -114,6 +115,7 @@ class FileController extends Controller
             $message->participation()->associate($participation);
             $message->save(); }
         
+            dd("ok");
             session()->flash('success', 'Ta participation a bien été uploadée !');
             return redirect()->route('home');
         }
