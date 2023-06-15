8491c7733170a7677b3a6b0705faafe3be44897e
Nisa Azizi <nisa.azizi@heig-vd.ch>
2023-06-15 11:13:10 +0200

    rm '

--- resources/js/components/Survey.vue
+++ resources/js/components/Survey.vue
@@ -63,7 +63,7 @@ export default {
         const end = new Date(start.getTime() + durée * 60000);
         const diff = end - now;
         const minutes = Math.floor((diff / 1000) / 60);
-        return `Plus que ${minutes} minutes !;
+        return `Plus que ${minutes} minutes !`;
 
     },
   sendChoice(item) {
