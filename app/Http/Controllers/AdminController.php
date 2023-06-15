5d8620d88b549de49541ae0f7c3b18470fe3fcc1
Loïc Fontaine <loic.fontaine@heig-vd.ch>
2023-06-15 13:52:10 +0200

    dd contest

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -65,6 +65,7 @@ class AdminController extends Controller
                 $participation->user;
             }
         }
+        dd($contests);
         //return response()->json(array("articles" => $articles, "polls" => $polls, "challenges" => $challenges, "contests" => $contests));
         return view("admin_dashboard", compact("articles", "poll", "challenge", "contests", "nbVotes"));
     }
