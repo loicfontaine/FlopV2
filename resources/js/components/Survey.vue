373fbee79535503dd33f7c1124badc2147784f76
Nisa Azizi <nisa.azizi@heig-vd.ch>
2023-06-15 10:42:38 +0200

    axios options

--- resources/js/components/Survey.vue
+++ resources/js/components/Survey.vue
@@ -64,11 +64,12 @@ export default {
       }
       const reponse = {
         sondage_id: item.id,
-        option_id: item.selectedOption
+        option_id: item.option.id
       };
       axios.post('/answer', reponse)
         .then(response => {
           console.log(response.data);
+          window.location.reload();
         })
         .catch(error => {
           console.error(error);
