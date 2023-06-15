2f1cba4cb81217ef2c6a99d37d46f0fc08694c4e
Nisa Azizi <nisa.azizi@heig-vd.ch>
2023-06-15 09:01:26 +0200

    add api

--- resources/js/components/Survey.vue
+++ resources/js/components/Survey.vue
@@ -1,11 +1,29 @@
 <template>
     <div>
-        <h1>Survey</h1>
     </div>
 </template>
-
 <script>
-    export default {
-        name: "Survey"
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      textField: ''
+    };
+  },
+  created() {
+    this.fetchData();
+  },
+  methods: {
+    async fetchData() {
+    try {
+      const response = await axios.get('https://flop-pingouin.heig-vd.ch/api/home');
+      this.data = response.data.sondages; // Assign the API response to the data property
+      console.log("api survey", this.data);
+    } catch (error) {
+      console.error(error);
     }
+  }
+  }
+};
 </script>
+
